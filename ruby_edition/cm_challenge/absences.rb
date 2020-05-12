b7dd635f8e0c037a72bec23e1164d6dbe8f95087
robocopkaka <robocopkaka@gmail.com>
2020-05-12 12:24:19 +0100

    export events from json files to iCalendar   - install iCalendar   - install pry   - do first export implementation to iCalendar   - create `invites` folder for storing `.ics` files   - add tests for #generate_events

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -1,9 +1,58 @@
 require_relative './api'
+require 'icalendar'
+require 'fileutils'
 
 module CmChallenge
   class Absences
     def to_ical
       # Your implementation here
+      absences = CmChallenge::Api.absences
+      members = CmChallenge::Api.members
+      cal = generate_events(absences, members)
+      generate_file(cal)
+    end
+
+    def generate_events(absences, members)
+      calendar = Icalendar::Calendar.new
+      # remove what looks like duplicate events
+      absences.uniq! { |mem| mem.values_at(:user_id, :start_date, :end_date) }
+      absences.each do |absence|
+        user_name = members.find { |mem| mem[:user_id] == absence[:user_id] }[:name]
+        calendar.add_event(create_event(user_name, absence))
+      end
+      calendar
+    end
+
+    private
+
+    def prettify_message(message)
+      message == "vacation" ? "is on vacation" : "is sick"
+    end
+
+    def generate_file(calendar)
+      FileUtils.mkdir_p "invites" unless Dir.exist?("invites")
+      filename = File.join("invites", "invites-#{Time.now}.ics")
+      File.open(filename, "w") { |f| f.write calendar.to_ical }
+    end
+
+    def create_event(user_name, absence)
+      event = Icalendar::Event.new
+      event.summary = "#{user_name} #{prettify_message(absence[:type])}"
+      event.categories = "NOT IN OFFICE"
+      event.dtstart = Icalendar::Values::Date.new(parse_date(absence[:start_date]))
+      event.dtend = Icalendar::Values::Date.new(parse_date(absence[:end_date]))
+      event
+    end
+
+    # doing this because Icalendar can't parse dates in example format, i.e "2017-01-01"
+    # returns an ICalendar DateTime object
+    def parse_date(date)
+      Icalendar::Values::DateOrDateTime.new(date)
+    rescue ArgumentError
+      p "Invalid date"
     end
   end
+
+  hero = CmChallenge::Absences.new
+  hero.to_ical
 end
--- ruby_edition/spec/absences_spec.rb
+++ ruby_edition/spec/absences_spec.rb
@@ -1,5 +1,22 @@
 require_relative '../cm_challenge/absences'
+require 'pry'
 
 RSpec.describe 'Absences' do
-  it { skip 'Your tests here' }
+  describe "#generate_events" do
+    let(:absences) { CmChallenge::Api.absences }
+    let(:members) { CmChallenge::Api.members }
+
+    it "returns a calender with events" do
+      events = CmChallenge::Absences.new.generate_events(absences, members)
+      expect(events).to be_kind_of Icalendar::Calendar
+      expect(events.prodid).to eq "icalendar-ruby"
+      expect(events.to_ical).to include "Mike is sick"
+      expect(events.to_ical).to include "Ines is on vacation"
+      expect(events.to_ical).to include "Daniel is on vacation"
+    end
+  end
+
+  describe  do
+
+  end
 end
