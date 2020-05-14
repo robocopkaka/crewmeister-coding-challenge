2f4207f634101ca1a6bad8851c90be1630c28b0e
robocopkaka <robocopkaka@gmail.com>
2020-05-14 12:19:21 +0100

    add form for downloads   - add initial routes implementation for downloads   - install shotgun

--- ruby_edition/web/index.rb
+++ ruby_edition/web/index.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 require 'erb'
-require_relative '../cm_challenge/absences'
+require 'pry'
+autoload :CmChallenge, './cm_challenge/api.rb'
+autoload :CmChallenge, './cm_challenge/absences.rb'
 
 set :views, settings.root
+set :port, 3000
 
 get '/' do
-  p "hereeee"
   erb :index
+end
+
+get '/download' do
+  CmChallenge::Absences.new.to_ical
+  send_file Dir["invites/**/*.ics"].last
 end
