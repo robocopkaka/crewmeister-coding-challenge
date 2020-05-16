69bbc7b6994982882a5f9ac241675c1d02f5e680
robocopkaka <robocopkaka@gmail.com>
2020-05-16 04:37:33 +0100

    cleanup ruby code   - fix style warnings from rubocop   - remove duplicated code

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -1,16 +1,24 @@
+# frozen_string_literal: true
+
 require_relative './api'
 require 'icalendar'
 require 'fileutils'
 
 module CmChallenge
+  # absences class - contains methods needed to create an ics export
+  # of absences
   class Absences
-    def initialize(user_id: nil , start_date: nil, end_date: nil)
+    def initialize(user_id: nil, start_date: nil, end_date: nil)
       @user_id = user_id
       @start_date = start_date
       @end_date = end_date
-      @absences = CmChallenge::Api.absences
+      # remove what looks like duplicate events
+      @absences = CmChallenge::Api.absences.uniq! do |mem|
+        mem.values_at(:user_id, :start_date, :end_date)
+      end
       @members = CmChallenge::Api.members
     end
+
     def to_ical
       # Your implementation here
       cal = add_events_to_calendar
@@ -19,12 +27,10 @@ module CmChallenge
 
     def add_events_to_calendar
       calendar = Icalendar::Calendar.new
-      # remove what looks like duplicate events
-      @absences.uniq! { |mem| mem.values_at(:user_id, :start_date, :end_date) }
       absences = filter_absences_by_dates(filter_absences_by_user(@absences))
       absences.each do |absence|
-        user_name = @members.find { |mem| mem[:user_id] == absence[:user_id] }[:name]
-        calendar.add_event(create_event(user_name, absence))
+        user = @members.find { |mem| mem[:user_id] == absence[:user_id] }
+        calendar.add_event(create_event(user[:name], absence))
       end
       calendar
     end
@@ -32,33 +38,36 @@ module CmChallenge
     private
 
     def prettify_message(message)
-      message == "vacation" ? "is on vacation" : "is sick"
+      message == 'vacation' ? 'is on vacation' : 'is sick'
     end
 
     def generate_file(calendar)
       # avoid creating empty files
       if calendar.events.count.zero?
-        puts "No events found matching parameters"
-        return "No events found matching parameters"
+        puts 'No events found matching parameters'
+        return 'No events found matching parameters'
       end
 
-      FileUtils.mkdir_p "invites" unless Dir.exist?("invites")
-      filename = File.join("invites", "invites-#{Time.now}.ics")
-      File.open(filename, "w") { |f| f.write calendar.to_ical }
+      FileUtils.mkdir_p 'invites' unless Dir.exist?('invites')
+      filename = File.join('invites', "invites-#{Time.now}.ics")
+      File.open(filename, 'w') { |f| f.write calendar.to_ical }
     end
 
     def create_event(user_name, absence)
       event = Icalendar::Event.new
       event.summary = "#{user_name} #{prettify_message(absence[:type])}"
-      event.categories = "NOT IN OFFICE"
-      event.dtstart = Icalendar::Values::Date.new(parse_date(absence[:start_date]))
-      event.dtend = Icalendar::Values::Date.new(parse_date(absence[:end_date]))
+      event.categories = 'NOT IN OFFICE'
+      event.dtstart =
+        Icalendar::Values::Date.new(parse_date(absence[:start_date]))
+      event.dtend =
+        Icalendar::Values::Date.new(parse_date(absence[:end_date]))
       event
     rescue ArgumentError
-      "Invalid date passed"
+      'Invalid date passed'
     end
 
-    # doing this because Icalendar can't parse dates in example format, i.e "2017-01-01"
+    # doing this because Icalendar can't parse datesin example format
+    # i.e "2017-01-01"
     # returns an ICalendar DateTime object
     def parse_date(date)
       Icalendar::Values::DateOrDateTime.new(date)
@@ -66,24 +75,33 @@ module CmChallenge
 
     def filter_absences_by_user(absences)
       return absences unless @user_id.to_s.present?
-      return absences&.select { |absence| absence[:user_id] == @user_id.to_i } if @user_id
+      return absences unless @user_id
 
-      absences
+      absences&.select { |absence| absence[:user_id] == @user_id.to_i }
     end
 
     def filter_absences_by_dates(absences)
-      return absences unless @start_date.to_s.present? && @end_date.to_s.present?
-
       if @start_date.to_s.present? && @end_date.to_s.present?
         return absences.select do |absence|
           absence[:start_date] >= @start_date && absence[:end_date] <= @end_date
         end
       end
+
       absences
     end
   end
 
-  # hero = CmChallenge::Absences.new(user_id: 644, start_date: "2017-03-10", end_date: "2017-03-20")
-  # hero = CmChallenge::Absences.new(user_id: 644, start_date: Time.now, end_date: Time.now + 1.hour)
+  # hero = CmChallenge::Absences
+  #        .new(
+  #          user_id: 644,
+  #          start_date: '2017-03-10',
+  #          end_date: '2017-03-20'
+  #        )
+  # hero = CmChallenge::Absences
+  #        .new(
+  #          user_id: 644,
+  #          start_date: Time.now,
+  #          end_date: Time.now + 1.hour
+  #        )
   # hero.to_ical
 end
--- ruby_edition/spec/absences_spec.rb
+++ ruby_edition/spec/absences_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../cm_challenge/absences'
 require 'pry'
 require 'fileutils'
@@ -7,62 +9,63 @@ RSpec.describe 'Absences' do
   let(:members) { CmChallenge::Api.members }
   let(:testing_instance) { CmChallenge::Absences.new }
 
-  describe "#add_events_to_calendar" do#
-    it "returns a calender with events" do
+  describe '#add_events_to_calendar' do
+    it 'returns a calender with events' do
       events = testing_instance.add_events_to_calendar
       expect(events).to be_kind_of Icalendar::Calendar
-      expect(events.prodid).to eq "icalendar-ruby"
-      expect(events.to_ical).to include "Mike is sick"
-      expect(events.to_ical).to include "Ines is on vacation"
-      expect(events.to_ical).to include "Daniel is on vacation"
+      expect(events.prodid).to eq 'icalendar-ruby'
+      expect(events.to_ical).to include 'Mike is sick'
+      expect(events.to_ical).to include 'Ines is on vacation'
+      expect(events.to_ical).to include 'Daniel is on vacation'
     end
   end
 
-  describe "create_event" do
+  describe 'create_event' do
     let(:absence) { absences.first }
-    it "creates an event" do
-      event = testing_instance.send(:create_event, "Kachi", absence)
+    it 'creates an event' do
+      event = testing_instance.send(:create_event, 'Kachi', absence)
       expect(event).to be_kind_of Icalendar::Event
-      expect(event.categories).to include "NOT IN OFFICE"
+      expect(event.categories).to include 'NOT IN OFFICE'
       expect(event.summary)
-        .to eq "Kachi #{testing_instance.send(:prettify_message, absence[:type])}"
+        .to eq "Kachi #{testing_instance
+                          .send(:prettify_message, absence[:type])}"
     end
 
-    it "should return an error if there's an invalid date" do
-      absence[:start_date] = "echo"
-      error = testing_instance.send(:create_event, "Kachi", absence)
-      expect(error).to eq "Invalid date passed"
+    it 'should return an error if there\'s an invalid date' do
+      absence[:start_date] = 'echo'
+      error = testing_instance.send(:create_event, 'Kachi', absence)
+      expect(error).to eq 'Invalid date passed'
     end
   end
 
-  describe "parse date" do
-    it "returns an Icalendar object if the date is valid" do
-      date = testing_instance.send(:parse_date, "2017-01-01")
+  describe 'parse date' do
+    it 'returns an Icalendar object if the date is valid' do
+      date = testing_instance.send(:parse_date, '2017-01-01')
       expect(date).to be_kind_of Icalendar::Values::DateOrDateTime
     end
   end
 
-  describe "#to_ical" do
+  describe '#to_ical' do
     before do
-      FileUtils.rm_rf Dir.glob("invites/**/*.ics") if Dir["invites"].present?
+      FileUtils.rm_rf Dir.glob('invites/**/*.ics') if Dir['invites'].present?
     end
     after do
-        FileUtils.rm_rf Dir.glob("invites/**/*.ics") if Dir["invites"].present?
+      FileUtils.rm_rf Dir.glob('invites/**/*.ics') if Dir['invites'].present?
     end
-    it "should return a file containing the events" do
+    it 'should return a file containing the events' do
       testing_instance.to_ical
-      expect(Dir["invites/**/*.ics"].count).to eq 1
+      expect(Dir['invites/**/*.ics'].count).to eq 1
     end
 
-    it "should return a message if there are no events" do
-      empty_instance = CmChallenge::Absences.new(user_id: 10000)
+    it 'should return a message if there are no events' do
+      empty_instance = CmChallenge::Absences.new(user_id: 100_00)
       error = empty_instance.to_ical
-      expect(error).to eq "No events found matching parameters"
+      expect(error).to eq 'No events found matching parameters'
     end
   end
 
-  describe "#filter_absences_by_user" do
-    it "returns only absences matching a given user id" do
+  describe '#filter_absences_by_user' do
+    it 'returns only absences matching a given user id' do
       user_id = members.first[:user_id]
       user_instance = CmChallenge::Absences.new(user_id: user_id)
       user_absences = user_instance.send(:filter_absences_by_user, absences)
@@ -71,19 +74,21 @@ RSpec.describe 'Absences' do
     end
   end
 
-  describe "#filter_absences_by_dates" do
-    it "returns only absences matching a given user id" do
-      start_date = "2017-03-10"
-      end_date = "2017-03-11"
+  describe '#filter_absences_by_dates' do
+    it 'returns only absences matching a given user id' do
+      start_date = '2017-03-10'
+      end_date = '2017-03-11'
       user_instance = CmChallenge::Absences.new(
         start_date: start_date,
         end_date: end_date
       )
       date_absences = user_instance.send(:filter_absences_by_dates, absences)
-      date_absences.map! { |absence| [absence[:start_date], absence[:end_date]] }
+      date_absences.map! do |absence|
+        [absence[:start_date], absence[:end_date]]
+      end
       date_absences.each do |absence|
         expect(absence[0]).to be >= start_date
-        expect(absence[1]).to be <=end_date
+        expect(absence[1]).to be <= end_date
       end
     end
   end
